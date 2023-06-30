861ae3350cb1acdd6889389e52f04f81ca35d218
Jorstev <71983628+Jorstev@users.noreply.github.com>
2023-06-30 12:16:17 -0600

    Trabajando en el backend
    Implementando thymeleaf en el landing page, la plantilla ya esta implementada

--- GameSphere/src/main/java/com/GameSphere/GameSphereApplication.java
+++ GameSphere/src/main/java/com/GameSphere/GameSphereApplication.java
@@ -2,8 +2,11 @@ package com.GameSphere;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.GameSphere.dao")
+
 public class GameSphereApplication {
 
 	public static void main(String[] args) {
