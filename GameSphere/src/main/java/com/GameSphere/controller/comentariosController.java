3dfb5207ee7b5c4754befc94e98a6d8ee848466b
Jorstev <71983628+Jorstev@users.noreply.github.com>
2023-08-19 11:49:57 -0600

    Fer: Finaliza comentarios y noticias

--- GameSphere/src/main/java/com/GameSphere/controller/comentariosController.java
+++ GameSphere/src/main/java/com/GameSphere/controller/comentariosController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -41,7 +42,7 @@ public class comentariosController {
         return "/comentarios/agregar";
     }
 
-    @GetMapping("/guardar")
+    @PostMapping ("/guardar")
     public String comentariosGuardar(comentarios Comentarios) {
         ComentariosService.saveC(Comentarios);
         return "redirect:/comentarios/comentarios";
